cbaaa342a654097c74d7e1f25899311a83885d36
Abdülselam GÜNDOĞDU <ali.koskocaman@gmail.com>
2020-11-12 14:03:47 +0300

    Delete git-gonder.sh

--- git-gonder.sh
+++
@@ -1,8 +0,0 @@
-git config --global user.name "Abdülselam GÜNDOĞDU"
-git config --global user.email "ali.koskocaman@gmail.com"
-
-git init
-git add --all
-git commit -m "First"
-git remote add origin https://github.com/AliKOCA/laravel8-generator.git
-git push -u origin master
